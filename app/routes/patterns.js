8ba791023cf3ca8048d95a65fbfb3d19ff8e72e3
Megan Bowra-Dean <megan@rabidtech.co.nz>
2015-10-03 01:08:18 +1300

    Some styling

--- app/routes/patterns.js
+++ app/routes/patterns.js
@@ -7,6 +7,11 @@ export default Ember.Route.extend({
         name: 'Baby Owl Ornaments',
         image: 'http://i.imgur.com/l3G5AYV.jpg',
         author: 'Josephine Wu'
+      },
+      {
+        name: 'Skull shawl/Döskallesjal',
+        image: 'http://images4.ravelrycache.com/uploads/FrkHansen/161263109/doskallesjal_medium.jpg',
+        author: 'kungen och majkis'
       }
     ];
   }
